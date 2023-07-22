e7f0042cf2cff19cf5483804f2e07e398c8ede41
Atul Kumar Tiwari <atulkumartiwari@Atuls-MacBook-Air.local>
2023-07-22 23:41:30 +0530

    learning js

---
+++ object1.js
@@ -0,0 +1,8 @@
+const a1 = {
+    name: "Atul",
+    section: 1,
+    isStudent: true
+}
+a1["friend"] = "Raj"
+a1["name"]= "Manav"
+console.log(a1)
---
+++ question2.js
@@ -0,0 +1,4 @@
+let a = "Harry"
+let b = 6
+console.log(a + b)
+console.log(typeof (a+b))
